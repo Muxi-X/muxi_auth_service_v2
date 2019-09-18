0f226431391f439054367104c3cfb7a7baaa5691
ShiinaOrez <shiina_orez@qq.com>
2019-09-18 21:26:49 +0800

    [fixed]

--- Makefile
+++ Makefile
@@ -7,7 +7,7 @@ gotool:
 	gofmt -w .
 	go tool vet . | grep -v vendor;true
 test:
-	@go test -v -count=1  ./...
+	@go test -v .
 ca:
 	openssl req -new -nodes -x509 -out conf/server.crt -keyout conf/server.key -days 3650 -subj "/C=DE/ST=NRW/L=Earth/O=Random Company/OU=IT/CN=127.0.0.1/emailAddress=xxxxx@qq.com"
 
---
+++ db.sql
@@ -0,0 +1,38 @@
+CREATE DATABASE `muxisite_auth` /*!40100 DEFAULT CHARACTER SET utf8mb4 */
+
+CREATE TABLE `roles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) DEFAULT NULL,
+  `default` tinyint(1) DEFAULT NULL,
+  `permissions` int(11) DEFAULT NULL,
+
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`),
+  KEY `ix_roles_default` (`default`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(164) DEFAULT NULL,
+  `info` text,
+  `username` varchar(164) DEFAULT NULL,
+  `avatar_url` text,
+  `personal_blog` text,
+  `github` text,
+  `flickr` text,
+  `weibo` text,
+  `zhihu` text,
+  `password_hash` varchar(164) DEFAULT NULL,
+  `role_id` int(11) DEFAULT NULL,
+  `birthday` varchar(164) DEFAULT NULL,
+  `group` varchar(164) DEFAULT NULL,
+  `hometown` varchar(164) DEFAULT NULL,
+  `left` tinyint(1) DEFAULT NULL,
+  `timejoin` varchar(164) DEFAULT NULL,
+  `timeleft` varchar(164) DEFAULT NULL,
+  `reset_t` varchar(164) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`),
+  KEY `role_id` (`role_id`),
+  CONSTRAINT `users_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=254 DEFAULT CHARSET=utf8;
